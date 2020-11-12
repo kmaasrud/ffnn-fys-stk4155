4cf2b6f7a59d70d53620a257c39808944fda0622
kmaasrud <kmaasrud@outlook.com>
2020-11-12 18:04:47 +0100

    Trying without vectorizing output and with another layer structure. Pushing to server for it to do the numerics

--- src/mnist_results/mnist_predict.py
+++ src/mnist_results/mnist_predict.py
@@ -12,7 +12,7 @@ from utils import split_and_scale, ReLU, leaky_ReLU
 
 out_dir = "../../doc/assets/"
 
-with open("mnist_array_output.pickle", "rb") as f:
+with open("mnist_scalar_output.pickle", "rb") as f:
     X_train, X_test, y_train, y_test = pickle.load(f)
 
 load_pickle = input("Load pickled networks (y/n)? ") == "y"
@@ -22,7 +22,7 @@ if load_pickle:
         nn_sigmoid = pickle.load(f)
 else:
     do_pickle = input("Pickle the neural networks (y/n)? ") == "y"
-    layer_structure = [X_train.shape[1], 100, y_train.shape[1]]
+    layer_structure = [X_train.shape[1], 200, 50, y_train.shape[1]]
     training_data = list(zip(X_train, y_train))
 
     nn_sigmoid = FFNN(layer_structure, epochs=20)
@@ -33,12 +33,12 @@ else:
             pickle.dump(nn_sigmoid, f)
             
 if do_plot:
-    y_predict_sigmoid_vector_form = nn_sigmoid.predict(X_test)
+    y_predict_sigmoid= nn_sigmoid.predict(X_test)
 
     # Reshape from vector y values into scalars
-    y_predict_sigmoid = np.zeros(y_predict_sigmoid_vector_form.shape[1])
-    for i, prediction in enumerate(y_predict_sigmoid_vector_form.T):
-        y_predict_sigmoid[i] = np.argmax(prediction)
+    # y_predict_sigmoid = np.zeros(y_predict_sigmoid_vector_form.shape[1])
+    # for i, prediction in enumerate(y_predict_sigmoid_vector_form.T):
+    #     y_predict_sigmoid[i] = np.argmax(prediction)
                 
     with open("mnist_X_test_unraveled.pickle", "rb") as f:
         X_test_imgs = pickle.load(f)
