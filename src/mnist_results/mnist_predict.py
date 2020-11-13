651a1e183d5bdced1b027cd5d339bb68e5fe7d49
kmaasrud <kmaasrud@outlook.com>
2020-11-13 10:55:02 +0100

    Pushing to server to try with ReLU

--- src/mnist_results/mnist_predict.py
+++ src/mnist_results/mnist_predict.py
@@ -12,6 +12,7 @@ from ffnn import FFNN
 from utils import split_and_scale, ReLU, leaky_ReLU
 
 out_dir = "../../doc/assets/"
+pickle_file = "network_relu.pickle"
 
 with open("mnist_array_output.pickle", "rb") as f:
     X_train, X_test, y_train, y_test = pickle.load(f)
@@ -19,27 +20,27 @@ with open("mnist_array_output.pickle", "rb") as f:
 load_pickle = input("Load pickled networks (y/n)? ") == "y"
 do_plot = input("Plot the predicted data? (y/n)? ") == "y"
 if load_pickle:
-    with open("network.pickle", "rb") as f:
-        nn_sigmoid = pickle.load(f)
+    with open(pickle_file, "rb") as f:
+        nn_relu = pickle.load(f)
 else:
     do_pickle = input("Pickle the neural networks (y/n)? ") == "y"
     layer_structure = [X_train.shape[1], 200, y_train.shape[1]]
     training_data = list(zip(X_train, y_train))
 
-    nn_sigmoid = FFNN(layer_structure)
-    nn_sigmoid.SGD_train(training_data)
+    nn_relu = FFNN(layer_structure, activation_function=ReLU)
+    nn_relu.SGD_train(training_data)
 
     if do_pickle:
-        with open("network.pickle", "wb") as f:
-            pickle.dump(nn_sigmoid, f)
+        with open(pickle_file, "wb") as f:
+            pickle.dump(nn_relu, f)
             
 if do_plot:
-    y_predict_sigmoid_vector_form = nn_sigmoid.predict(X_test)
+    y_predict_relu_vector_form = nn_relu.predict(X_test)
 
     # Reshape from vector y values into scalars
-    y_predict_sigmoid = np.zeros(y_predict_sigmoid_vector_form.shape[1])
-    for i, prediction in enumerate(y_predict_sigmoid_vector_form.T):
-        y_predict_sigmoid[i] = np.argmax(prediction)
+    y_predict_relu = np.zeros(y_predict_relu_vector_form.shape[1])
+    for i, prediction in enumerate(y_predict_relu_vector_form.T):
+        y_predict_relu[i] = np.argmax(prediction)
                 
     with open("mnist_X_test_unraveled.pickle", "rb") as f:
         X_test_imgs = pickle.load(f)
@@ -48,6 +49,6 @@ if do_plot:
     for i in range(9):
         plt.subplot(330 + 1 + i)
         plt.imshow(X_test_imgs[j+i], cmap=plt.get_cmap('gray'))
-        plt.title(y_predict_sigmoid[j+i])
+        plt.title(y_predict_relu[j+i])
 
-    plt.savefig(os.path.join(out_dir, "nn_sigmoid_mnist_plot.png"))
+    plt.savefig(os.path.join(out_dir, "nn_sigmoid_relu_plot.png"))
