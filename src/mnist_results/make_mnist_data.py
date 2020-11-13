daf4349b3384d7338d52ac0cbe739c147f34bc7f
kmaasrud <kmaasrud@outlook.com>
2020-11-13 08:23:17 +0100

    Plotted predicted data. Quite rubbish, but this utilizes sigmoid

--- src/mnist_results/make_mnist_data.py
+++ src/mnist_results/make_mnist_data.py
@@ -8,15 +8,21 @@ import pickle
 X_train = np.zeros((X_train_unraveled.shape[0], 28 * 28)); X_test = np.zeros((X_test_unraveled.shape[0], 28 * 28))
 for i, pic in enumerate(X_train_unraveled):
     X_train[i] = pic.ravel()
+    X_train[i] = [val/255 for val in X_train[i]]
 for i, pic in enumerate(X_test_unraveled):
     X_test[i] = pic.ravel()
+    X_test[i] = [val/255 for val in X_test[i]]
     
 # Reshape output data
 y_train = np.zeros((y_train_unshaped.shape[0], 10)); y_test = np.zeros((y_test_unshaped.shape[0], 10))
 for i, num in enumerate(y_train_unshaped):
-    y_train[i] = np.array([int(bool(num - 1 == j)) for j in range(10)])
+    y_train[i] = np.array([int(bool(num == j)) for j in range(10)])
 for i, num in enumerate(y_test_unshaped):
-    y_test[i] = np.array([int(bool(num - 1 == j)) for j in range(10)])
+    y_test[i] = np.array([int(bool(num == j)) for j in range(10)])
+    
+print(X_train[2])
+print(y_train_unshaped[2])
+print(y_train[2])
     
 with open("mnist_array_output.pickle", "wb") as f:
     pickle.dump((X_train, X_test, y_train, y_test), f)
--- src/mnist_results/mnist_predict.py
+++ src/mnist_results/mnist_predict.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pickle
+import random
 
 import sys, os, inspect
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
@@ -12,20 +13,20 @@ from utils import split_and_scale, ReLU, leaky_ReLU
 
 out_dir = "../../doc/assets/"
 
-with open("mnist_scalar_output.pickle", "rb") as f:
+with open("mnist_array_output.pickle", "rb") as f:
     X_train, X_test, y_train, y_test = pickle.load(f)
 
 load_pickle = input("Load pickled networks (y/n)? ") == "y"
-do_plot = input("Plot the predicted data? (y/n)?") == "y"
+do_plot = input("Plot the predicted data? (y/n)? ") == "y"
 if load_pickle:
     with open("network.pickle", "rb") as f:
         nn_sigmoid = pickle.load(f)
 else:
     do_pickle = input("Pickle the neural networks (y/n)? ") == "y"
-    layer_structure = [X_train.shape[1], 200, 50, y_train.shape[0]]
+    layer_structure = [X_train.shape[1], 200, y_train.shape[1]]
     training_data = list(zip(X_train, y_train))
 
-    nn_sigmoid = FFNN(layer_structure, epochs=20)
+    nn_sigmoid = FFNN(layer_structure)
     nn_sigmoid.SGD_train(training_data)
 
     if do_pickle:
@@ -33,19 +34,20 @@ else:
             pickle.dump(nn_sigmoid, f)
             
 if do_plot:
-    y_predict_sigmoid= nn_sigmoid.predict(X_test)
+    y_predict_sigmoid_vector_form = nn_sigmoid.predict(X_test)
 
     # Reshape from vector y values into scalars
-    # y_predict_sigmoid = np.zeros(y_predict_sigmoid_vector_form.shape[1])
-    # for i, prediction in enumerate(y_predict_sigmoid_vector_form.T):
-    #     y_predict_sigmoid[i] = np.argmax(prediction)
+    y_predict_sigmoid = np.zeros(y_predict_sigmoid_vector_form.shape[1])
+    for i, prediction in enumerate(y_predict_sigmoid_vector_form.T):
+        y_predict_sigmoid[i] = np.argmax(prediction)
                 
     with open("mnist_X_test_unraveled.pickle", "rb") as f:
         X_test_imgs = pickle.load(f)
 
-    for i in range(9):  
+    j = random.randint(0,9990)
+    for i in range(9):
         plt.subplot(330 + 1 + i)
-        plt.imshow(X_test_imgs[i], cmap=plt.get_cmap('gray'))
-        plt.title(y_predict_sigmoid[i])
+        plt.imshow(X_test_imgs[j+i], cmap=plt.get_cmap('gray'))
+        plt.title(y_predict_sigmoid[j+i])
 
-    plt.show()
+    plt.savefig(os.path.join(out_dir, "nn_sigmoid_mnist_plot.png"))
